2de76f2caf4456808d652c1dc819b7721bc36c77
Zach-Mac <zach.macdonald.021@gmail.com>
2023-10-07 20:58:27 -0400

    draggable reorder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@invictus.codes/nuxt-vuetify'],
+	modules: ['@invictus.codes/nuxt-vuetify', '@vueuse/nuxt', '@formkit/auto-animate'],
 	ssr: false,
 	devtools: {
 		enabled: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,60 @@
 <script setup lang="ts">
-const states = ['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']
-const countries = ref([] as string[])
+import { VImg } from 'vuetify/lib/components/index.mjs'
+import { vAutoAnimate } from '@formkit/auto-animate/vue'
+import draggable from 'vuedraggable'
 
-// const getImage = (code: string) => `@/assets/flags/png250px/${code}.png`
+interface Country {
+	code: string
+	name: string
+}
 
-const imgWidth = ref(250)
+const params = useUrlSearchParams('history')
+
+const selectedCountries = ref([] as Country[])
+// const selectedCountries = ref([] as string[])
+if (params?.countries && typeof params.countries == 'string') {
+	const codes = params.countries.split('-')
+
+	selectedCountries.value = codes.map((code, index) => ({
+		code,
+		name: codeToName[code]
+	}))
+}
+watch(selectedCountries, value => {
+	// params.countries = value.join('-')
+	params.countries = value.map(obj => obj.code).join('-')
+})
+
+const imgHeight = ref(250)
 
 function zoomOut() {
-	imgWidth.value = imgWidth.value - 100 || 0
+	imgHeight.value = imgHeight.value - 100 || 0
 }
 function zoomIn() {
-	imgWidth.value = imgWidth.value + 100 || 100
+	imgHeight.value = imgHeight.value + 100 || 100
 }
+
+const img = ref<VImg[]>([])
+
+function getWidth(index: number) {
+	// w = nw/nh * h
+	const natWidth = img.value[index]?.naturalWidth ?? 1
+	const natHeight = img.value[index]?.naturalHeight ?? 1
+	const height = img.value[index]?.height ?? 1
+
+	const width = (natWidth / natHeight) * Number(height)
+	return width
+}
+
+const dragOptions = computed(() => ({
+	animation: 100,
+	group: 'group',
+	disabled: false,
+	ghostClass: 'ghost',
+	sort: true
+}))
+
+const reorderMode = ref(false)
 </script>
 
 <template>
@@ -19,9 +62,12 @@ function zoomIn() {
 		<v-row>
 			<v-col>
 				<v-autocomplete
-					v-model="countries"
+					v-auto-animate
+					v-model="selectedCountries"
+					return-object
 					auto-select-first
 					clearable
+					closable-chips
 					chips
 					multiple
 					label="Countries"
@@ -29,41 +75,75 @@ function zoomIn() {
 					item-title="name"
 					item-value="code"
 					variant="outlined"
-				></v-autocomplete>
+				>
+					<template #append>
+						<v-btn
+							@click="reorderMode = !reorderMode"
+							:variant="reorderMode ? 'outlined' : 'elevated'"
+							>Reorder</v-btn
+						>
+					</template>
+				</v-autocomplete>
+
 				<div class="d-flex flex-wrap">
 					<v-card
-						v-for="code in countries"
+						v-if="!reorderMode"
+						v-for="(country, index) in selectedCountries"
 						:elevation="5"
-						class="d-flex flex-column"
+						class="d-flex flex-column ma-1"
 						:rounded="0"
 						hover
 					>
 						<v-img
-							:width="imgWidth"
-							:src="`/flags/png1000px/${code.toLowerCase()}.png`"
-							:alt="code"
+							class="elevation-5"
+							:ref="el => (img[index] = el)"
+							:height="imgHeight"
+							:width="getWidth(index)"
+							:src="`/flags/png1000px/${country.code.toLowerCase()}.png`"
+							:alt="country.name"
 						/>
-						<p class="align-self-center">{{ code }}</p>
+						<p class="align-self-center">{{ country.name }}</p>
 					</v-card>
+
+					<draggable
+						v-else
+						v-model="selectedCountries"
+						item-key="code"
+						v-bind="dragOptions"
+					>
+						<template #item="{ element }">
+							<v-card
+								:elevation="5"
+								class="d-flex flex-column ma-1"
+								:rounded="0"
+								hover
+							>
+								<v-img
+									class="elevation-5"
+									:width="100"
+									:src="`/flags/png100px/${element.code.toLowerCase()}.png`"
+									:alt="element.name"
+								/>
+								<p class="align-self-center">{{ element.name }}</p>
+							</v-card>
+						</template>
+					</draggable>
 				</div>
 			</v-col>
 		</v-row>
-		<v-row>
-			<v-col :cols="4">
-				<v-card :elevation="2" class="float-bottom">
-					<v-slider
-						class="mt-4 mb-0 mp-0"
-						v-model="imgWidth"
-						:min="10"
-						:max="1000"
-						append-icon="mdi-magnify-plus-outline"
-						prepend-icon="mdi-magnify-minus-outline"
-						@click:append="zoomIn"
-						@click:prepend="zoomOut"
-					></v-slider>
-				</v-card>
-			</v-col>
-		</v-row>
+
+		<v-card :elevation="2" class="float-bottom">
+			<v-slider
+				class="mt-4 mb-0 mp-0"
+				v-model="imgHeight"
+				:min="10"
+				:max="500"
+				append-icon="mdi-magnify-plus-outline"
+				prepend-icon="mdi-magnify-minus-outline"
+				@click:append="zoomIn"
+				@click:prepend="zoomOut"
+			></v-slider>
+		</v-card>
 	</v-container>
 </template>
 
